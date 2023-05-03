228731ed4196fddf9c7492108e8ae464b672e14a
Cameron Murison <cmuris4@gmail.com>
2023-05-03 09:30:35 +0100

    msuic_library test diriving routes with databases

---
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+
+  context "GET /albums" do
+    it "returns a list of all albums" do
+      response = get('/albums')
+      expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+end
